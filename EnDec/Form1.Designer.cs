694bd4d7d9c72381d0be76350fa3f2dff703c5c7
unknown <Administrator@LUMIA.(none)>
2012-06-08 09:50:50 +0700

    First commit succes run minimal feature of encrypt-decrypt using md5

---
+++ EnDec/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EnDec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtToEnc = new System.Windows.Forms.TextBox();
+            this.cmdEncResult = new System.Windows.Forms.Label();
+            this.txtEncResult = new System.Windows.Forms.TextBox();
+            this.cmdToEnc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtToEnc
+            // 
+            this.txtToEnc.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtToEnc.Location = new System.Drawing.Point(0, 0);
+            this.txtToEnc.Multiline = true;
+            this.txtToEnc.Name = "txtToEnc";
+            this.txtToEnc.Size = new System.Drawing.Size(263, 336);
+            this.txtToEnc.TabIndex = 0;
+            // 
+            // cmdEncResult
+            // 
+            this.cmdEncResult.AutoSize = true;
+            this.cmdEncResult.Location = new System.Drawing.Point(306, 46);
+            this.cmdEncResult.Name = "cmdEncResult";
+            this.cmdEncResult.Size = new System.Drawing.Size(53, 13);
+            this.cmdEncResult.TabIndex = 4;
+            this.cmdEncResult.Text = "Decrypt >";
+            this.cmdEncResult.Click += new System.EventHandler(this.cmdEncResult_Click);
+            // 
+            // txtEncResult
+            // 
+            this.txtEncResult.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txtEncResult.Location = new System.Drawing.Point(440, 0);
+            this.txtEncResult.Multiline = true;
+            this.txtEncResult.Name = "txtEncResult";
+            this.txtEncResult.Size = new System.Drawing.Size(263, 336);
+            this.txtEncResult.TabIndex = 2;
+            // 
+            // cmdToEnc
+            // 
+            this.cmdToEnc.AutoSize = true;
+            this.cmdToEnc.Location = new System.Drawing.Point(307, 70);
+            this.cmdToEnc.Name = "cmdToEnc";
+            this.cmdToEnc.Size = new System.Drawing.Size(52, 13);
+            this.cmdToEnc.TabIndex = 3;
+            this.cmdToEnc.Text = "< Encrypt";
+            this.cmdToEnc.Click += new System.EventHandler(this.cmdToEnc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 336);
+            this.Controls.Add(this.cmdToEnc);
+            this.Controls.Add(this.txtEncResult);
+            this.Controls.Add(this.cmdEncResult);
+            this.Controls.Add(this.txtToEnc);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtToEnc;
+        private System.Windows.Forms.Label cmdEncResult;
+        private System.Windows.Forms.TextBox txtEncResult;
+        private System.Windows.Forms.Label cmdToEnc;
+    }
+}
+
---
+++ EnDec/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using Microsoft.VisualBasic; 
+
+namespace EnDec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdEncResult_Click(object sender, EventArgs e)
+        {
+            txtEncResult.Text = Decrypt(txtToEnc.Text.Trim(), "&%#@?,:*");
+        }
+
+        private void cmdToEnc_Click(object sender, EventArgs e)
+        {
+            txtToEnc.Text = Encrypt(txtEncResult.Text.Trim(), "&%#@?,:*");
+        }
+
+        public string Decrypt(string pText, string pDecrKey)
+        {
+            string str = "";
+            byte[] rgbIV = new byte[] { 0x12, 0x34, 0x56, 120, 0x90, 0xab, 0xcd, 0xef };
+            byte[] buffer = new byte[pText.Length + 1];
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(Strings.Left(pDecrKey, 8));
+                DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+                buffer = Convert.FromBase64String(pText);
+                MemoryStream stream2 = new MemoryStream();
+                CryptoStream stream = new CryptoStream(stream2, provider.CreateDecryptor(bytes, rgbIV), CryptoStreamMode.Write);
+                stream.Write(buffer, 0, buffer.Length);
+                stream.FlushFinalBlock();
+                str = Encoding.UTF8.GetString(stream2.ToArray());
+            }
+            catch (Exception exception1)
+            {
+                str = "Decrypt error" + exception1.StackTrace; 
+            }
+            return str;
+        }
+         
+        public string Encrypt(string pText, string pEncrKey)
+        {
+            string message = "";
+            byte[] rgbIV = new byte[] { 0x12, 0x34, 0x56, 120, 0x90, 0xab, 0xcd, 0xef };
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(  Strings.Left(pEncrKey, 8));
+                byte[] buffer = Encoding.UTF8.GetBytes(pText);
+                DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+                MemoryStream stream2 = new MemoryStream();
+                CryptoStream stream = new CryptoStream(stream2, provider.CreateEncryptor(bytes, rgbIV), CryptoStreamMode.Write);
+                stream.Write(buffer, 0, buffer.Length);
+                stream.FlushFinalBlock();
+                message = Convert.ToBase64String(stream2.ToArray());
+            }
+            catch (Exception exception1)
+            {
+                message = "Encrypt error" + exception1.StackTrace; 
+            }
+            return message;
+        }
+
+    }
+}
